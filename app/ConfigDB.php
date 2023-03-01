ded6b03a778bd3e7c3135f49f10b1a42268f54d0
erickgonzalez01 <erickjoelg@gmail.com>
2023-03-01 12:54:54 -0600

    01/03/2023 Configurando conecion a base de datos

--- app/ConfigDB.php
+++ app/ConfigDB.php
@@ -6,7 +6,7 @@ use mysqli;
 class ConfigDB{
     public static function Get(){          
         try {           
-            $mysql = new mysqli("localhost","root","","citas");
+            $mysql = new mysqli("localhost","root","rty$%jhu*HuNh4","citas");
             return $mysql;
         } catch (Exception $th) {
             return $th;
